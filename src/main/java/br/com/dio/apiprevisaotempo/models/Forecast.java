4e8b245fedad5e17a0ee8faaa1fb98b5de1ce52a
Eder Sant'ana <edersana@gmail.com>
2022-01-21 19:06:37 -0300

    tests included

--- src/main/java/br/com/dio/apiprevisaotempo/models/Forecast.java
+++ src/main/java/br/com/dio/apiprevisaotempo/models/Forecast.java
@@ -6,12 +6,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import lombok.Builder;
 import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({ "date", "max", "min" })
 @Generated("jsonschema2pojo")
 @Data
+@Builder
 public class Forecast {
 
 	@JsonProperty("date")
--- src/test/java/br/com/dio/apiprevisaotempo/ApiPrevisaoTempoApplicationTests.java
+++ src/test/java/br/com/dio/apiprevisaotempo/ApiPrevisaoTempoApplicationTests.java
@@ -1,13 +1,54 @@
 package br.com.dio.apiprevisaotempo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.dio.apiprevisaotempo.connectors.HGBrasilConnector;
+import br.com.dio.apiprevisaotempo.models.Forecast;
+import br.com.dio.apiprevisaotempo.resources.PrevisaoTempoResource;
+import br.com.dio.apiprevisaotempo.services.PrevisaoTempoService;
 
 @SpringBootTest
 class ApiPrevisaoTempoApplicationTests {
+	
+	@Autowired
+	PrevisaoTempoResource resource;
 
+	@Autowired
+	HGBrasilConnector connector;
+
+	@Autowired
+	PrevisaoTempoService service;
+	
 	@Test
-	void contextLoads() {
+	void testFetchWeatherForCity() {
+		
+		var resultadoApi = connector.fetchWeatherForCity("Sao Paulo,SP");
+		assertEquals("São Paulo", resultadoApi.getCityName());
+	}
+	
+	@Test
+	void testFetchPrevisaoTempo(){
+		var resultadoService = service.fetchPrevisaoTempo("Sao Paulo,SP");
+		assertEquals("São Paulo", resultadoService.getNomeCidade());
 	}
 
+	@Test
+	void testGetPrevisaoTempo(){
+		var resultadoResource = resource.get("Sao Paulo,SP");
+		assertNotEquals(null, resultadoResource);;
+	}
+	
+	
+	@Test
+	void testForecast(){
+		Forecast forecast = Forecast.builder().date("20/12/2021").min(22).max(30).build();
+		
+		assertEquals(Integer.valueOf(30), forecast.getMax());
+	}
+	
 }
